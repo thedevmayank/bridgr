6bc78e549851d7bd99b2380e40256ab3be9eba55
thedevmayank <dadhichmayank09@gmail.com>
2025-12-18 12:19:30 +0530

    updated

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -16,9 +16,9 @@ export default function About() {
   return (
     <>
       <div id='about' className='scroll-mt-50 relative  border-b-2 border-gray-400  '>
-        <Image src={aboutpic} alt="About Image" className="w-full h-auto " />
-        <div className='absolute top-15 lg:left-115 xl:left-130 2xl:left-150 md:left-80 sm:left-60 left-40 '>
-            <div data-aos="fade-up">   <h2 className='md:text-4xl font-bold text-black  top-10 left-10 '>Bridgr - Library Manager App</h2></div>
+        <Image src={aboutpic} alt="About Image" className="w-full object-center" />
+        <div className='absolute top-5 lg:left-115 xl:left-120 2xl:left-120 md:left-60 sm:left-60 left-19 '>
+            <div data-aos="fade-up">   <h2 className=' sm:text-2xl md:text-4xl font-bold text-black top-0 md:top-10 md:left-10  '>Bridgr - Library Manager App</h2></div>
 
      
         </div>
