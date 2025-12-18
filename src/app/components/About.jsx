0c6fedc7494fb5604b44930391f945e22203b6c1
thedevmayank <dadhichmayank09@gmail.com>
2025-12-18 13:02:44 +0530

    update size of mobile pic

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -16,9 +16,9 @@ export default function About() {
   return (
     <>
       <div id='about' className='scroll-mt-50 relative  border-b-2 border-gray-400  '>
-        <Image src={aboutpic} alt="About Image" className="w-full  object-cover object-top overflow-hidden h-200 md:h-full" />
+        <Image src={aboutpic}  alt="About Image" className="w-full  object-cover object-top overflow-hidden h-150 md:h-full" />
         <div className='absolute top-5 lg:left-115 xl:left-120 2xl:left-120 md:left-60 sm:left-60 left-19  '>
-            <div data-aos="fade-up">   <h2 className=' sm:text-2xl md:text-4xl font-bold text-black top-0 md:top-10 md:left-10  '>Bridgr - Library Manager App</h2></div>
+            <div data-aos="fade-up">   <h2 className=' sm:text-2xl md:text-4xl font-bold text-black top-0 md:top-10 md:left-10  '>Bridgr  Library Manager App</h2></div>
 
      
         </div>
